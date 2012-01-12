7f7d2855b9f82c8e29cc6250a864b4f9b1d2d7ff
MacBookPro <macbookpro@MacBookProteki-MacBook-Pro.local>
2012-01-12 11:32:23 +0800

    Initial Commit

---
+++ ThreeSixtyForIPad/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ThreeSixtyForIPad
+//
+//  Created by MacBookPro on 12/1/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
